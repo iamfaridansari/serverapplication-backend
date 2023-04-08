5f977f232bcb70b8bc78836ab9ab6453c98c604a
Farid Ansari <iamfaridansari@gmail.com>
2023-04-08 17:11:08 +0530

    update

--- model/nightsuitSchema.js
+++ model/nightsuitSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 const nightsuitproductsSchema = new mongoose.Schema({
   name: String,
@@ -10,8 +11,80 @@ const nightsuitproductsSchema = new mongoose.Schema({
   fabric: String,
 });
 
+const nightsuituserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  addressList: [
+    {
+      address: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+nightsuituserSchema.methods.generateToken = async function () {
+  const token = jwt.sign({ _id: this._id }, process.env.secretkey);
+  this.tokens = this.tokens.concat({ token: token });
+  await this.save();
+  return token;
+};
+
+const nightsuitaddressSchema = new mongoose.Schema({
+  house: String,
+  state: String,
+  city: String,
+  landmark: String,
+  pincode: Number,
+});
+
+const nightsuitcouponSchema = new mongoose.Schema({
+  name: String,
+  discount: Number,
+  type: String,
+});
+
+//
 const nightsuitproducts = mongoose.model(
   "nightsuitproducts",
   nightsuitproductsSchema
 );
-module.exports = nightsuitproducts;
+const nightsuituser = mongoose.model("nightsuituser", nightsuituserSchema);
+const nightsuitaddress = mongoose.model(
+  "nightsuitaddress",
+  nightsuitaddressSchema
+);
+const nightsuitcoupon = mongoose.model(
+  "nightsuitcoupon",
+  nightsuitcouponSchema
+);
+
+module.exports = {
+  nightsuitproducts,
+  nightsuituser,
+  nightsuitaddress,
+  nightsuitcoupon,
+};
--- routes/nightsuitRoute.js
+++ routes/nightsuitRoute.js
@@ -1,5 +1,10 @@
 const router = require("express").Router();
-const nightsuitproducts = require("../model/nightsuitSchema");
+const {
+  nightsuitproducts,
+  nightsuituser,
+  nightsuitaddress,
+  nightsuitcoupon,
+} = require("../model/nightsuitSchema");
 const multer = require("multer");
 const upload = multer({ dest: "./uploads" });
 const multipleUpload = upload.fields([
@@ -56,4 +61,158 @@ router.post("/api/delete/nightsuit/products", async (req, res) => {
   res.status(200).json({ message: "Product deleted" });
 });
 
+//
+router.post("/api/signup/nightsuit/user", async (req, res) => {
+  const { name, email, mobile, password } = req.body;
+  if (!name || !email || !mobile || !password) {
+    return res.status(422).json({ message: "Enter complete details" });
+  }
+  const userExist = await nightsuituser.findOne({
+    email: email,
+  });
+  if (userExist) {
+    return res.status(422).json({ message: "Email already in use" });
+  } else {
+    try {
+      const newuser = new nightsuituser({ name, email, mobile, password });
+      await newuser.save();
+      res
+        .status(200)
+        .json({ message: "Account created. Please login to continue" });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
+
+router.post("/api/login/nightsuit/user", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(422).json({ message: "Enter complete details" });
+  }
+  const userExist = await nightsuituser.findOne({ email: email });
+  if (!userExist) {
+    return res.status(422).json({ message: "User does not exist" });
+  }
+  if (userExist) {
+    if (userExist.password !== password) {
+      return res
+        .status(422)
+        .json({ message: "Invalid email address or password" });
+    } else {
+      const token = await userExist.generateToken();
+      res.status(200).json({ email: email, password: password, token: token });
+    }
+  }
+});
+
+//
+router.post("/api/post/nightsuit/address", async (req, res) => {
+  const { id, name, email, mobile, house, state, city, landmark, pincode } =
+    req.body;
+  if (!house || !state || !city || !pincode) {
+    return res.status(422).json({ message: "Enter complete details" });
+  } else {
+    try {
+      const newAddress = new nightsuitaddress({
+        house,
+        state,
+        city,
+        landmark,
+        pincode,
+      });
+      await newAddress.save();
+      res.status(200).json({ message: "Address saved" });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
+
+router.get("/api/get/nightsuit/address", async (req, res) => {
+  const addressList = await nightsuitaddress.find();
+  if (addressList) {
+    res.status(200).json(addressList);
+  } else {
+    return res.status(422).json({ message: "Failed to fetch Address" });
+  }
+});
+
+router.delete("/api/delete/nightsuit/address", async (req, res) => {
+  const { id } = req.body;
+  try {
+    await nightsuitaddress.findByIdAndDelete(id);
+    res.status(200).json({ message: "Address deleted" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//
+router.post("/api/post/nightsuit/coupon", async (req, res) => {
+  const { name, discount, type } = req.body;
+  if (!name || !discount || !type) {
+    return res.status(422).json({ message: "Enter complete details" });
+  } else {
+    try {
+      const newCoupon = new nightsuitcoupon({ name, discount, type });
+      await newCoupon.save();
+      res.status(200).json({ message: "Coupon saved" });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
+
+router.get("/api/get/nightsuit/coupon", async (req, res) => {
+  try {
+    const couponList = await nightsuitcoupon.find();
+    if (couponList) {
+      res.status(200).json(couponList);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.delete("/api/delete/nightsuit/coupon", async (req, res) => {
+  const { id } = req.body;
+  try {
+    await nightsuitcoupon.findByIdAndDelete(id);
+    res.status(200).json({ message: "Coupon deleted" });
+  } catch (error) {}
+});
+
+router.post("/api/apply/nightsuit/coupon", async (req, res) => {
+  const { coupon, subtotal } = req.body;
+  //
+  let newSubTotal = 0;
+  if (!coupon || !subtotal) {
+    return res.status(422).json({ message: "Failed to apply coupon" });
+  }
+  if (subtotal < 500) {
+    return res
+      .status(422)
+      .json({ message: "Coupon applicable on orders above 499 only" });
+  }
+  //
+  const couponExist = await nightsuitcoupon.findOne({ name: coupon });
+  if (!couponExist) {
+    return res.status(422).json({ message: "Invalid coupon" });
+  }
+  if (couponExist) {
+    if (couponExist.name === coupon && couponExist.type === "rupees") {
+      newSubTotal = subtotal - couponExist.discount;
+    } else if (couponExist.name === coupon && couponExist.type === "percent") {
+      newSubTotal = Math.round(
+        subtotal - (subtotal * couponExist.discount) / 100
+      );
+    }
+    res.status(200).json({
+      message: "Coupon applied",
+      coupon: coupon,
+      subtotal: newSubTotal,
+    });
+  }
+});
 module.exports = router;
